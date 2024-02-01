c037fc859e586d26ae8a94db899fe7128bb92f90
fivecong <fivesun1228@163.com>
2024-02-01 18:05:37 +0800

    update进度条颜色

--- src/components/progressBar/index.tsx
+++ src/components/progressBar/index.tsx
@@ -11,8 +11,8 @@ export default function ProgressBar() {
     //     NProgress.done();
     // }, [pathname, router]);
     return <TopLoaderBar
-        height="0.25rem"
-        color="#fffd00"
+        height="0.15rem"
+        color="linear-gradient(45deg, #f10006, #ef5b00, #e59c01, #19ca05, #00cab5, #0264c8, #c303c3)"
         options={{ showSpinner: false }}
         shallowRouting
     />
